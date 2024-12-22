950504fb2cad409a68b41268e096b53c514b2fc9
Shawn Jacobson <shawnjacobson33@gmail.com>
2024-12-21 23:04:23 -0600

    Adds more notes on ending solution of ordinary least-squares algorithm with matrix inversion.

--- LinearRegression.py
+++ LinearRegression.py
@@ -65,12 +65,62 @@
 # ----- Opposite of Singular Matrix
 # --- if the features are too correlated than regularization can be used to reduce
 # penalize large coefficients.
+# --- Determinant:
+# ----- A value that represents the scalar product of the matrix values. How much information can you gain
+# from each of them separately. Marks linear independence, and reliability of coefficients. If the determinant
+# is close to zero (near singular) then the coefficients will become exponentially large and unstable when
+# we take the inverse of X@X^T
 
-
-# CODE:
+# LEAST-SQUARES CODE:
 # - must use np.array([...]) or a container class that implements
 # the __matul__ dunder to use the '@' operator between two object types.
 
 import numpy as np
 
-print(np.array([1, 2, 3]) @ np.array([4, 5, 6]))
+size = (100, 5)
+
+X = np.random.randint(1, 10, size=size)
+Y = np.random.randint(2, 4, size=(size[0],)) * X[:, 0] + np.random.randint(5, 7, size=(size[0],)) * X[:, 1]
+
+# IMPORTANT: scale your features because you need the feature interaction vectors be accurate representations
+# of the relationships between them. Shouldn't be biased by different units or scales. Will lead to more
+# accurate determinants when computing the inverse of the gram matrix, and by consequence better "B"
+# model coefficients.
+X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+
+# the design matrix representing how features relate to each other
+# too dependent (multi-collinear), or independent and well-spread?
+# goal is non-singularity
+# each outputted value is a scalar product representing the similarity between rows
+# -- cosine similarity
+# when data is scaled by Z-Score then if two feature values in different rows are similar then
+# we can either get a negative * negative = positive OR positive * positive = positive, so values that are
+# larger (more positive) have low correlation (linearly independent), otherwise if a value has a negative
+# Z-Score (lower than the mean) * positive Z-Score then there will be a small value outputted (not similar)
+# ...so negative values are good!
+gram_mx = X.T @ X
+
+# next take the inverse to flatten the values back into its original form, so we can recover the
+# unique B model coefficients. Scales back each feature according to its variance, reflecting the individual
+# contributions of each feature.
+# -- the diagonal elements of the inverse will represent the adjustment to the variance of a feature
+# with regard to the correlations to the other features.
+# -- scales by the determinant, which can be found by computing cofactors for each element in A, which
+# represents the contribution of each feature to the rest of the matrix minus its row and column
+# 1 / det(A) * adj(A) the adjugate matrix which contains the cofactors (determinant * alternating negative pattern)
+
+# -- rank: number of linearly independent rows or columns in a matrix. If a matrix has full rank then
+# its rank is equal to its smallest dimension.
+
+inv_gram_mx = np.linalg.inv(gram_mx)
+
+
+# then once we have a representation of how much unique information each feature contributes then
+# we can take the product of that with the interaction between the feature columns and labels
+# This is a computationally expensive closed-form solution where the gram matrix is O(p^3)
+# must be "well-conditioned" (means non-singular, little multi-collinearity) and with a "p" (feature set)
+# < 1000.
+result = inv_gram_mx @ (X.T @ Y)
+
+# otherwise if feature set is greater than 1000 and < 10,000 then you can use a "Cholesky" decomposition
+#
